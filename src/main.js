b5556631317a6f1dc35a76effa4b2ec03d1f018d
damian <damian.szalimow@onet.eu>
2025-01-17 09:08:31 +0100

    migration to vite.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+import './styles/style.scss'
+
+document.querySelector('#app').innerHTML = `
+    <div class="container-sm text-center">
+      <div class="row">
+        <div class="col">
+          <div>
+            <label for="inputBarcode" class="form-label">Type your barcode</label>
+            <input type="email" class="form-control" id="inputBarcode" aria-describedby="barcodeHelp" placeholder="5900541000017">
+            <div id="barcodeHelp" class="form-text">for test purposes input only EAN-13 code</div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col output">
+        </div>
+      </div>
+    </div>
+`
 let barcode = "" // test barcode '5900541000017' żywiec zdrój 
 
 const container = document.querySelector('.output')
@@ -9,14 +29,13 @@ brand.classList.add('brand')
 
 container.after(brand)
 
-
 btn.addEventListener('click', (e)=> {
-    e.preventDefault();
-    barcode = intput.value +""
-    fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
-    .then(res => res.json())
-    .then(res => {
-        console.log(res.product.brands);
-        brand.innerText = `BRAND of barcode product is ${res.product.brands}`
-    })
+  e.preventDefault();
+  barcode = intput.value +""
+  fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
+  .then(res => res.json())
+  .then(res => {
+      console.log(res.product.brands);
+      brand.innerText = `BRAND of barcode product is ${res.product.brands}`
+  })
 })
